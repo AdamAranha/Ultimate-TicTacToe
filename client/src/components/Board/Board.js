8d1563fa7348a5985893662a675050d9f5972f98
Adam <aearanha@hotmail.com>
2021-07-13 08:53:51 -0400

    Created specific id for child boxes and able to log them onClick

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -1,20 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Board.css';
 
 export default function Board({ boxContent, parent }) {
 
+    const [childId, setChildId] = useState('')
     let thisRef = useRef();
     let boardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     let parentArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
+
+    useEffect(() => {
+        if (!parent) {
+            setChildId(thisRef.current.parentNode.parentNode.id)
+        }
+    }, [])
+
     function target(event) {
-        console.log(event.target.parentNode.parentNode.id)
+        console.log(event.target.id)
     }
     return (
         <div className='board-container'>
             {boardArray.map(item => (
-                <div className={parent ? `board-box board-box-${item} parent` : `board-box board-box-${item} child`} id={parent ? `${parentArray[item]}` : `${parentArray[item]}${item}`} key={`box${item}`} onClick={
-                    target} >
+                <div className={parent ? `board-box board-box-${item} parent` : `board-box board-box-${item} child`} id={parent ? `${parentArray[item]}` : `${childId}${item}`} key={`box${item}`}
+                    onClick={parent ? null : (event) => target(event)} ref={parent ? null : thisRef} >
                     {boxContent}
                 </div>
             ))}
