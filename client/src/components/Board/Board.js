1e7cd67c38ec4e6b0d8a4dde1ab239aebf4d5167
Adam <aearanha@hotmail.com>
2021-07-15 17:12:34 -0400

    Changed board/code written out instead of replicating

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -4,26 +4,35 @@ import './Board.css';
 export default function Board({ boxContent, parent }) {
 
     const [childId, setChildId] = useState('')
+    const [position, setPosition] = useState([
+
+    ])
+
     let thisRef = useRef();
-    let boardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    let parentArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    const boardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const parentArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+    let input = <div className='board-input'>Hi</div>;
+    let output = "<div className='board-output'>Bye</div>";
 
 
     useEffect(() => {
         if (!parent) {
             setChildId(thisRef.current.parentNode.parentNode.id)
         }
+
     }, [])
 
     function target(event) {
         console.log(event.target.id)
+        event.target.innerHTML = output
     }
+
     return (
         <div className='board-container'>
             {boardArray.map(item => (
                 <div className={parent ? `board-box board-box-${item} parent` : `board-box board-box-${item} child`} id={parent ? `${parentArray[item]}` : `${childId}${item}`} key={`box${item}`}
                     onClick={parent ? null : (event) => target(event)} ref={parent ? null : thisRef} >
-                    {boxContent}
+                    {parent ? boxContent : position}
                 </div>
             ))}
         </div>
