eb72ff13b0778849a91b2c4eba25744a3e5d6228
Adam <aearanha@hotmail.com>
2021-07-31 18:00:41 -0400

    Removes hover effects on game end

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -6,7 +6,8 @@ function Board() {
     useEffect(() => {
         let classname = document.getElementsByClassName('childchild');
         Object.keys(classname).forEach(element => {
-            classname[element].addEventListener('click', registerClick, true)
+            classname[element].addEventListener('click', registerClick, true);
+            classname[element].className += ' vacant';
         });
     })
     // placementArray keeps track of x's and o's on the board
@@ -22,11 +23,12 @@ function Board() {
 
     function registerClick(event) {
         let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
-        console.log(event)
         if (placementArray[boardArray.indexOf(event.target.parentNode.parentNode.id)][numberArray.indexOf(event.target.id)] === 0) {
             placementArray[boardArray.indexOf(event.target.parentNode.parentNode.id)][numberArray.indexOf(event.target.id)] = isPlayer ? 2 : 1
-            // console.table(placementArray)
             setBoard()
+            //Changes hover colour of squares from green to red when they become occupied
+            let classList = document.getElementById(event.target.parentNode.parentNode.id).childNodes[0].childNodes[numberArray.indexOf(event.target.id)].className
+            document.getElementById(event.target.parentNode.parentNode.id).childNodes[0].childNodes[numberArray.indexOf(event.target.id)].className = classList.replace('vacant', 'occupied')
             isPlayer = !isPlayer
         } else {
             console.log("Space occupied")
@@ -41,71 +43,93 @@ function Board() {
                     square === 0 ? '' : square === 1 ? '<span class="marker">O</span>' : '<span class="marker">X</span>'
             })
         })
-        checkForWin();
+        checkSectionWin();
     }
 
-    function checkForWin() {
-        let currentPlayer = isPlayer ? 2 : 1
-        let isWin;
-
+    function checkSectionWin() {
         placementArray.forEach((array, index) => {
             //Ignores array that has already been won
             if (excludeArray.includes(index)) {
                 return;
             }
-            //Horizontal Win Conditions
-            if ((array[0] === currentPlayer &&
-                array[1] === currentPlayer &&
-                array[2] === currentPlayer)
-                ||
-                (array[3] === currentPlayer &&
-                    array[4] === currentPlayer &&
-                    array[5] === currentPlayer)
-                ||
-                (array[6] === currentPlayer &&
-                    array[7] === currentPlayer &&
-                    array[8] === currentPlayer)
-                ||
-                //Vertical Win Conditions
-                (array[0] === currentPlayer &&
-                    array[3] === currentPlayer &&
-                    array[6] === currentPlayer)
-                ||
-                (array[1] === currentPlayer &&
-                    array[4] === currentPlayer &&
-                    array[7] === currentPlayer)
-                ||
-                (array[2] === currentPlayer &&
-                    array[5] === currentPlayer &&
-                    array[8] === currentPlayer)
-                ||
-                //Vertical Win Conditions
-                (array[0] === currentPlayer &&
-                    array[4] === currentPlayer &&
-                    array[8] === currentPlayer)
-                ||
-                (array[2] === currentPlayer &&
-                    array[4] === currentPlayer &&
-                    array[6] === currentPlayer)) {
-
+            if (checkForWin(array)) {
                 console.log('DING DING DING')
-                //Blurs sector on win
                 changeDisplay(index)
-                // Remove onClick from all squares in sector on win
                 removeHandlers(index)
                 excludeArray.push(index)
+                overBoard[index] = isPlayer ? 2 : 1
+                checkOverboardWin()
+                console.log(overBoard)
             }
         })
     }
 
-    function removeHandlers(sector) {
+    function checkOverboardWin() {
+        if (checkForWin(overBoard)) {
+            console.log("WE HAVE A WINNER!!")
+            boardArray.forEach((array, index) => removeHandlers(index))
 
-        console.log(boardArray[sector])
-        document.getElementById(boardArray[sector]).childNodes[0].childNodes.forEach(item => {
+            let classname = document.getElementsByClassName('childchild');
+            Object.keys(classname).forEach(element => {
+                classname[element].classList.remove('vacant')
+                classname[element].classList.remove('occupied')
+
+            });
+        }
+
+    }
+
+    function checkForWin(array) {
+        let currentPlayer = isPlayer ? 2 : 1
+
+        //Horizontal Win Conditions
+        if ((array[0] === currentPlayer &&
+            array[1] === currentPlayer &&
+            array[2] === currentPlayer)
+            ||
+            (array[3] === currentPlayer &&
+                array[4] === currentPlayer &&
+                array[5] === currentPlayer)
+            ||
+            (array[6] === currentPlayer &&
+                array[7] === currentPlayer &&
+                array[8] === currentPlayer)
+            ||
+            //Vertical Win Conditions
+            (array[0] === currentPlayer &&
+                array[3] === currentPlayer &&
+                array[6] === currentPlayer)
+            ||
+            (array[1] === currentPlayer &&
+                array[4] === currentPlayer &&
+                array[7] === currentPlayer)
+            ||
+            (array[2] === currentPlayer &&
+                array[5] === currentPlayer &&
+                array[8] === currentPlayer)
+            ||
+            //Vertical Win Conditions
+            (array[0] === currentPlayer &&
+                array[4] === currentPlayer &&
+                array[8] === currentPlayer)
+            ||
+            (array[2] === currentPlayer &&
+                array[4] === currentPlayer &&
+                array[6] === currentPlayer)) {
+            return true
+        } else {
+            return false
+        }
+
+    }
+
+    function removeHandlers(section) {
+        document.getElementById(boardArray[section]).childNodes[0].childNodes.forEach(item => {
+
             item.removeEventListener('click', registerClick, true)
         })
     }
-
+    //Blurs sector on win and shows winning symbol
     function changeDisplay(index) {
         let currentPlayer = isPlayer ? 2 : 1
 
@@ -118,7 +142,7 @@ function Board() {
         <div className="bigBoard">
             <div id="A" className="one child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -137,7 +161,7 @@ function Board() {
             </div>
             <div id="B" className="two child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -156,7 +180,7 @@ function Board() {
             </div>
             <div id="C" className="three child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -175,7 +199,7 @@ function Board() {
             </div>
             <div id="D" className="four child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -194,7 +218,7 @@ function Board() {
             </div>
             <div id="E" className="five child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -213,7 +237,7 @@ function Board() {
             </div>
             <div id="F" className="six child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -232,7 +256,7 @@ function Board() {
             </div>
             <div id="G" className="seven child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -251,7 +275,7 @@ function Board() {
             </div>
             <div id="H" className="eight child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
@@ -270,7 +294,7 @@ function Board() {
             </div>
             <div id="I" className="nine child">
                 <div className="smallBoard-container">
-                    <div id={`zero`} className=" one childchild"></div>
+                    <div id={`zero`} className="one childchild"></div>
                     <div id="one" className="two childchild"></div>
                     <div id="two" className="three childchild"></div>
                     <div id="three" className="four childchild"></div>
