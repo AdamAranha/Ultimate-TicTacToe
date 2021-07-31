7abc05d1fc19a37d753fa0a07d1a0b471d917e6c
Adam <aearanha@hotmail.com>
2021-07-31 15:43:07 -0400

    Sectors change appearance when the sector is won

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -6,9 +6,7 @@ function Board() {
     useEffect(() => {
         let classname = document.getElementsByClassName('childchild');
         Object.keys(classname).forEach(element => {
-            classname[element].addEventListener('click', register, true)
-            // console.log(key);        // the name of the current key.
-            // console.log(classname[key]); // the value of the current key.
+            classname[element].addEventListener('click', registerClick, true)
         });
     })
     // placementArray keeps track of x's and o's on the board
@@ -16,13 +14,13 @@ function Board() {
     for (let count = 0; count < 9; count++) {
         placementArray[count] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
-
+    let overBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     let boardArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
     let isPlayer = true
     let excludeArray = [];
 
 
-    let register = function registerClick(event) {
+    function registerClick(event) {
         let numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
         console.log(event)
         if (placementArray[boardArray.indexOf(event.target.parentNode.parentNode.id)][numberArray.indexOf(event.target.id)] === 0) {
@@ -90,31 +88,31 @@ function Board() {
                     array[4] === currentPlayer &&
                     array[6] === currentPlayer)) {
 
-                isWin = console.log('DING DING DING')
+                console.log('DING DING DING')
                 //Blurs sector on win
-                document.querySelector(`#${boardArray[index]}`).className += ' blur'
-                // console.log(document.querySelector(`#${boardArray[index]}`).childNodes[0].childNodes)
-                console.log(index)
+                changeDisplay(index)
                 // Remove onClick from all squares in sector on win
                 removeHandlers(index)
                 excludeArray.push(index)
-                console.log(excludeArray)
-
-
-            } else {
             }
         })
-        return isWin
     }
 
     function removeHandlers(sector) {
 
         console.log(boardArray[sector])
         document.getElementById(boardArray[sector]).childNodes[0].childNodes.forEach(item => {
-            item.removeEventListener('click', register, true)
+            item.removeEventListener('click', registerClick, true)
         })
     }
 
+    function changeDisplay(index) {
+        let currentPlayer = isPlayer ? 2 : 1
+
+        document.querySelector(`#${boardArray[index]}`).childNodes[0].className += ' blur'
+        // document.querySelector('.win-X').style.display = 'flex'
+        document.querySelector(`#${boardArray[index]}`).childNodes[currentPlayer === 2 ? 1 : 2].style.display = 'flex'
+    }
 
     return (
         <div className="bigBoard">
@@ -130,6 +128,12 @@ function Board() {
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
                 </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
+                </div>
             </div>
             <div id="B" className="two child">
                 <div className="smallBoard-container">
@@ -142,6 +146,12 @@ function Board() {
                     <div id="six" className="seven childchild"></div>
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
+                </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
                 </div>
             </div>
             <div id="C" className="three child">
@@ -156,6 +166,12 @@ function Board() {
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
                 </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
+                </div>
             </div>
             <div id="D" className="four child">
                 <div className="smallBoard-container">
@@ -168,6 +184,12 @@ function Board() {
                     <div id="six" className="seven childchild"></div>
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
+                </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
                 </div>
             </div>
             <div id="E" className="five child">
@@ -182,6 +204,12 @@ function Board() {
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
                 </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
+                </div>
             </div>
             <div id="F" className="six child">
                 <div className="smallBoard-container">
@@ -194,6 +222,12 @@ function Board() {
                     <div id="six" className="seven childchild"></div>
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
+                </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
                 </div>
             </div>
             <div id="G" className="seven child">
@@ -208,6 +242,12 @@ function Board() {
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
                 </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
+                </div>
             </div>
             <div id="H" className="eight child">
                 <div className="smallBoard-container">
@@ -221,6 +261,12 @@ function Board() {
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
                 </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
+                </div>
             </div>
             <div id="I" className="nine child">
                 <div className="smallBoard-container">
@@ -233,6 +279,12 @@ function Board() {
                     <div id="six" className="seven childchild"></div>
                     <div id="seven" className="eight childchild"></div>
                     <div id="eight" className="nine childchild"></div>
+                </div>
+                <div className="win">
+                    X
+                </div>
+                <div className="win">
+                    O
                 </div>
             </div>
 
