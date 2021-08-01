03f29465634424dc3347c7285ea1b09a00e85f62
Adam <aearanha@hotmail.com>
2021-08-01 01:31:47 -0400

    Added some colour to opponent text

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,12 +4,12 @@ import './Header.css'
 
 export default function Header() {
 
-    const [opponent, setOpponent] = useState('vs. Player 2')
+    const [opponent, setOpponent] = useState(null)
 
     function showOpponent() {
 
         if (opponent === 'vs. Player 2') {
-            setOpponent('vs. Computer')
+            setOpponent('vs. AI')
         } else {
             setOpponent('vs. Player 2')
         }
@@ -17,7 +17,7 @@ export default function Header() {
 
     return <>
         <Container fluid className='pt-4 px-3'>
-            <h2 className='title'>Ultimate Tic Tac Toe <span>{opponent}</span></h2>
+            <h2 className='title'>Ultimate Tic Tac Toe <span id="opponent-text">{opponent}</span></h2>
             <button onClick={() => showOpponent()}>Click</button>
             <Alert variant="success">
                 <Alert.Heading>Hey, nice to see you</Alert.Heading>
