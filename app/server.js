478a02f253fd76c7400201656357381c209a4b42
Adam <aearanha@hotmail.com>
2021-08-03 10:40:28 -0400

    Heroku deploying, add app.get

--- app/server.js
+++ app/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
+
 io.on('connection', (socket) => {
     socket.emit('id', socket.id)
     console.log('New User connected')
