29b32b992cb6d44451437c5bbde962fffdc393f3
Adam <aearanha@hotmail.com>
2021-08-02 22:05:22 -0400

    Working on socketio s

--- app/server.js
+++ app/server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const app = express();
-// const PORT = process.env.
+const { v4: uuidv4 } = require('uuid');
+const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
@@ -9,8 +8,17 @@ const io = require('socket.io')(http, {
     }
 })
 
+let placementArray = [];
+for (let count = 0; count < 9; count++) {
+    placementArray[count] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
 io.on('connection', (socket) => {
+    socket.emit('id', socket.id)
     console.log('New User connected')
+    // socket.join(uuidv4())
+    console.log(socket.id)
+    console.log(socket.rooms)
 })
 
 http.listen(5000, () => { console.log('Listening on http://localhost:5000') })
