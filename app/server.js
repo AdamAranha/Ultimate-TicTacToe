dc6625e5447153a19edc91a5e8b8634b0af97a73
Adam <aearanha@hotmail.com>
2021-08-04 10:05:26 -0400

    Added function to return board state, added conditions for displaying site in production

--- app/server.js
+++ app/server.js
@@ -8,29 +8,43 @@ const io = require('socket.io')(http, {
         methods: ["GET", "POST"]
     }
 })
-
 const PORT = process.env.PORT || 5000
+const STATIC_PATH = process.env.ENV === 'production' ? path.join(__dirname, '../client/build') : path.join(__dirname, '../client/public')
+
+let placementArray = [];
+for (let count = 0; count < 9; count++) {
+    placementArray[count] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}) // Not working on heroku
+if (process.env.ENV === 'production') {
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    })
+    console.log('Make sure the react build is up to date!!')
+}
 
-// app.get('*', (req, res) => {
-//     res.send('Hello World')
-// }) // This works in heroku
-
-
-
-
+function checkAvailability(position) {
+    if (placementArray[position.p1][position.p2] === 0) {
+        placementArray[position.p1][position.p2] = position.currentPlayer
+    } else {
+        console.log('Space occupied')
+    }
+    return placementArray
+}
 
 io.on('connection', (socket) => {
     socket.emit('id', socket.id)
     console.log('New User connected')
-    // socket.join(uuidv4())
-    console.log(socket.id)
-    console.log(socket.rooms)
+
+    socket.on('requestPosition', (position, callback) => {
+        console.log(position)
+        placementArray = [...checkAvailability(position)]
+        callback({
+            placementArray
+        })
+    })
 })
 
-http.listen(PORT, () => { console.log('Listening on http://localhost:5000') })
+http.listen(PORT, () => { console.log(`Listening on http://localhost:${PORT}`) })
