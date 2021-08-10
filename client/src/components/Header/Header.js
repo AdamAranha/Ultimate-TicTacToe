778b5e42d444379587ea2f4b80e467de38f17659
Adam <aearanha@hotmail.com>
2021-08-10 14:15:16 -0400

    Created modal component, filter when modal appears

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Container, Alert } from 'react-bootstrap';
 import './Header.css'
 import { ReactComponent as LogoShort } from './ttt-logo.svg'
 import { ReactComponent as LogoLong } from './ttt-logo-long.svg'
 import { ReactComponent as HamburgerMenu } from './hamburger-menu.svg'
+import Modal from '../Modal/Modal.js'
 
 
 
 export default function Header() {
 
     const [opponent, setOpponent] = useState('Program')
+    const [showModal, setShowModal] = useState(false)
+    const avoidRef1 = useRef();
+    const avoidRef2 = useRef();
+
+
+    function toggle() {
+        setShowModal(!showModal)
+    }
 
     function showOpponent() {
 
@@ -29,10 +38,13 @@ export default function Header() {
                 <LogoShort className='header-logo-short' onClick={() => console.log('Clicking on the SVG works')} />
                 <h2 id='opponent-text'>{opponent}</h2>
             </div>
+            <Modal show={showModal} close={toggle} showOpponent={showOpponent} avoid1={avoidRef1} avoid2={avoidRef2} className='app-modal' message='You are about to start a new game, current game progress will be lost.' />
+
             <div className='whatever'>
-                <button className='header-changeGamemode' onClick={() => showOpponent()}>Change Gamemode</button>
-                <HamburgerMenu className="header-hamburger-menu" onClick={() => showOpponent()} />
+                <button ref={avoidRef1} className='header-changeGamemode' onClick={() => toggle()}>Change Gamemode</button>
+                <HamburgerMenu ref={avoidRef2} className="header-hamburger-menu" onClick={() => toggle()} />
             </div>
+
         </div>
 
     </>
---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+import './Modal.css'
+
+export default function Modal({ message, show, close, showOpponent, avoid1, avoid2 }) {
+
+    const modelRef = useRef()
+
+    useEffect(() => {
+        function handleClick(event) {
+            if (modelRef.current && !modelRef.current.contains(event.target) && !avoid1.current.contains(event.target) && !avoid2.current.contains(event.target)) {
+                close()
+            }
+        }
+        // add when mounted
+        document.addEventListener("mousedown", handleClick);
+        // return function to be called when unmounted
+        return () => {
+            document.removeEventListener("mousedown", handleClick);
+        };
+    })
+
+
+
+    return <>
+        {
+            show ?
+                <div>
+                    <div className='modal-dimScreen'></div>
+                    <div className='modal-container' ref={modelRef}>
+                        <p className='modal-text'>{message}</p>
+                        <div className='modal-buttonGroup'>
+                            <button className='button' onClick={() => {
+                                close()
+                                showOpponent()
+                            }}>Ok</button>
+                            <button className='button' onClick={() => close()}>Cancel</button>
+                        </div>
+
+                    </div>
+                </div>
+
+                : null}
+    </>
+}
