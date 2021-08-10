6f06b90a0e05bce143cb654451a0f2a0e993b9e0
Adam <aearanha@hotmail.com>
2021-08-10 10:33:23 -0400

    Logo and hamburger change depending on mobile or desktop view

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Alert } from 'react-bootstrap';
 import './Header.css'
-import { ReactComponent as YourSvg } from './ttt-logo.svg'
+import { ReactComponent as LogoShort } from './ttt-logo.svg'
 import { ReactComponent as LogoLong } from './ttt-logo-long.svg'
 import { ReactComponent as HamburgerMenu } from './hamburger-menu.svg'
 
@@ -25,13 +25,13 @@ export default function Header() {
     return <>
         <div className='header-container'>
             <div className='logo'>
-                {/* <LogoLong /> */}
-                <YourSvg onClick={() => console.log('Clicking on the SVG works')} />
+                <LogoLong className='header-logo-long' />
+                <LogoShort className='header-logo-short' onClick={() => console.log('Clicking on the SVG works')} />
                 <h2 id='opponent-text'>{opponent}</h2>
             </div>
             <div className='whatever'>
-                {/* <button onClick={() => showOpponent()}>Change Gamemode</button> */}
-                <HamburgerMenu id="hamburger-menu" onClick={() => showOpponent()} />
+                <button className='header-changeGamemode' onClick={() => showOpponent()}>Change Gamemode</button>
+                <HamburgerMenu className="header-hamburger-menu" onClick={() => showOpponent()} />
             </div>
         </div>
 
