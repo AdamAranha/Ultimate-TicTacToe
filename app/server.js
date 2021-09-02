882e95696284a183166f8446c69228ca4b481095
Adam <aearanha@hotmail.com>
2021-09-02 01:05:03 -0400

    Added handler for click event

--- app/server.js
+++ app/server.js
@@ -31,103 +31,15 @@ io.on('connection', (socket) => {
     // console.log(io.sockets.adapter.rooms)
     // issocket.to(socket.id).emit('greeting', message)
 
-    socket.on('reset', (position, callback) => {
-        const placementArray = resetBoard()
-        callback({
-            placementArray
-        })
-    })
+
 
     socket.on('disconnect', () => {
         console.log(`User disconnected [${socket.id}]`)
     })
 
-    socket.on('getRooms', (e) => {
-        // console.log('Redo called')
-        console.log(io.sockets.adapter.rooms)
+    socket.on('surpriseBitch', (message) => {
+        console.log(message)
     })
-
-    socket.on('logId', () => {
-
-    })
-
-
-
-    socket.on('searchUser', (query) => {
-        if (io.sockets.adapter.rooms.has(query)) {
-            io.sockets.to(query).emit('joinRequest', socket.id)
-            console.log('User found')
-        } else { console.log('User not found') }
-    })
-
-    socket.on('challengeAccepted', (values) => {
-        socket.join(values.challenger)
-        socket.leave(socket.id)
-
-        startGame(io.sockets.adapter.rooms.get(values.challenger))
-    })
-
-
-    function readyPlayer(roomName, currentPlayer) {
-        //Add event listeners for the currentPlayer
-        console.log('Running readyPlayer one ' + roomName + ',' + currentPlayer)
-        // socket.emit('id', socket.id)
-        // io.sockets.to(roomName).emit(socket.id.toString(), 'addEventListeners')
-        io.sockets.to(roomName).emit('testBroadcast', {
-            currentPlayer,
-            command: 'addEventListeners'
-        })
-    }
-
-    function startGame(room) {
-
-        let setToArray = [...room]
-
-        let roomName = setToArray[0]
-        let coinFlip = Math.round(Math.random());
-        let firstPlayer = setToArray[coinFlip];
-        setToArray.forEach((item) => item !== firstPlayer ? secondPlayer = item : null)
-        let alternate = 0;
-        let currentPlayer = firstPlayer;
-
-
-        console.log(`Gameset,\nRoom Name:[${roomName}]\nFirst Player:[${firstPlayer}]\nSecond Player:[${secondPlayer}]\nCurrent Player:[${currentPlayer}]]`)
-
-        readyPlayer(roomName, currentPlayer)
-
-
-
-
-        socket.on('requestPosition', (position, callback) => {
-            console.log(`Click registered by ${currentPlayer}`)
-            console.log(`Requesting placement at [${position.p1}][${position.p2}]`)
-            const { placementArray, wasPlaced } = checkAvailability(position)
-
-            console.log(wasPlaced)
-            if (wasPlaced) {
-                alternate++
-                if (alternate % 2 === 0) {
-                    currentPlayer = firstPlayer
-                } else {
-                    currentPlayer = secondPlayer
-                }
-                readyPlayer(roomName, currentPlayer)
-            } else {
-                console.log('calculating')
-            }
-
-
-
-            callback({
-                placementArray, wasPlaced
-            })
-        })
-
-
-    }
-
-
 })
-
 
 http.listen(PORT, () => { console.log(`Listening on http://localhost:${PORT}`) })
