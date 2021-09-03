c8cb1cc207fb1dea55e3aaa85eef0f6874024d84
Adam <aearanha@hotmail.com>
2021-09-02 20:54:27 -0400

    Moved Board logic here

--- client/src/components/Board/boardUtil.js
+++ client/src/components/Board/boardUtil.js
@@ -1,9 +1,29 @@
+
+const sectionArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+let gameBoard = [];
+
+
 const boardUtil = {
+
     registerClick: function (event, socket) {
         console.log(event.target.id)
-        socket.emit('surpriseBitch', event.target.id)
-    }
-    ,
+        socket.emit('requestPosition', {
+            position: event.target.id,
+            user: socket.id
+        })
+    },
+
+    setBoard: function (board) {
+        let gameBoard = [...board]
+        gameBoard.forEach((section, sectionIndex) => {
+            section.forEach((square, squareIndex) => {
+                document.querySelector(`#${sectionArray[sectionIndex]}-${squareIndex}`).innerHTML =
+                    square === 0 ? null :
+                        square === 1 ? '<span class="markerO">X</span>' : '<span class="markerX">O</span>'
+            })
+        })
+    },
+
     strikeThrough: function (array) {
         switch (array) {
             case "A": return <div><div id="h-top"></div><div id="v-left"></div><div id="d-ltr"></div></div>;
