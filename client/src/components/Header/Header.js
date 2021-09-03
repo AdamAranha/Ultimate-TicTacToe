f3ce0d0bd9173ad8b403e300a18c9286f7dcd27d
Adam <aearanha@hotmail.com>
2021-09-03 01:47:16 -0400

    Gutted User content, added modal components for gamemode switch and first player choice

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,24 +11,28 @@ import Modal from '../Modal/Modal.js'
 export default function Header({ socket }) {
 
     const [opponent, setOpponent] = useState('Program')
-    const [showModal, setShowModal] = useState(false)
-    const [searchUser, setSearchUser] = useState('')
-    const [modalSetting, setModalSetting] = useState('gamemode')
-    const [challenger, setChallenger] = useState(null)
-    const avoidRef1 = useRef();
-    const avoidRef2 = useRef();
+    const [firstPlayer, setFirstPlayer] = useState(null)
+    const [showModal_1, setShowModal_1] = useState(false)
+    const [showModal_2, setShowModal_2] = useState(false)
 
     useEffect(() => {
-        // socket.on('joinRequest', (value) => {
-        //     setModalSetting('joinRequest')
-        //     console.log('Challenge issued')
-        //     setChallenger(value)
-        //     toggle()
-        // })
+        socket.on('starting', () => {
+            if (opponent === 'Program') { toggle(1) }
+        })
     })
 
-    function toggle() {
-        setShowModal(!showModal)
+
+    function toggle(num) {
+        switch (num) {
+            case 1:
+                setShowModal_1(!showModal_1)
+                break;
+            case 2:
+                setShowModal_2(!showModal_2)
+                break;
+            default:
+                break;
+        }
     }
 
     function showOpponent() {
@@ -42,9 +46,15 @@ export default function Header({ socket }) {
         }
     }
 
-    function handleChange(event) {
-        setSearchUser(event.target.value)
+    function startGame(firstPlayer) {
+        setFirstPlayer(firstPlayer);
+        if (firstPlayer) {
+            console.log(firstPlayer)
+            socket.emit('startGame', firstPlayer)
+        }
     }
+
+
 
     return <>
         <div className='header-container'>
@@ -53,27 +63,26 @@ export default function Header({ socket }) {
                 <LogoShort className='header-logo-short' onClick={() => console.log('Clicking on the SVG works')} />
                 <h2 id='opponent-text'>{opponent}</h2>
             </div>
-            <Modal challenger={challenger} modalSetting={modalSetting} socket={socket} show={showModal} close={toggle} showOpponent={showOpponent} avoid1={avoidRef1} avoid2={avoidRef2} className='app-modal' message='You are about to start a new game, current game progress will be lost.' />
+            <Modal showModal={showModal_1} close={() => toggle(1)}>
+                <div>Who do you want to go first?</div>
+                <button className='modal-options-button' onClick={() => { toggle(1); startGame('Player') }}>You</button>
+                <button className='modal-options-button' onClick={() => { toggle(1); startGame('Random') }}>Random</button>
+                <button className='modal-options-button' onClick={() => { toggle(1); startGame('Computer') }}>Computer</button>
+            </Modal>
 
-            <div className='header-fullscreen-gameOptions'>
-                {opponent === 'User' ?
-                    <div className='header-userOptions'>
-                        <div className='header-id'>
-                            {/* ID:{socket.id} */}
-                        </div>
-                        <form className='header-search'>
-                            <input type='text' className='header-searchInput' value={searchUser} onChange={(event) => handleChange(event)} placeholder='Search for a friend'></input>
-                            <button className='header-searchButton' onClick={(event) => {
-                                event.preventDefault()
-                                // socket.emit('searchUser', searchUser)
-                            }}>Search</button>
-                        </form>
-                    </div>
-                    : null}
-                <button ref={avoidRef1} className='header-changeGamemode' onClick={() => toggle()}>Change Gamemode</button>
-            </div>
+            <Modal showModal={showModal_2} close={() => toggle(2)}>
+                <p>Are you sure about that?</p>
+                <button className='reset-button' onClick={() => toggle(2)}> NAH DAWG</button>
+                <button className='reset-button' onClick={() => {
+                    toggle(2);
+                    showOpponent();
+                }}> FO SHO</button>
+            </Modal>
+
+            <button className='header-changeGamemode' onClick={() => toggle(2)}>Change Gamemode</button>
+
             <div className='header-mobile-gameOptions'>
-                <HamburgerMenu ref={avoidRef2} className="header-hamburger-menu" onClick={() => toggle()} />
+                <HamburgerMenu className="header-hamburger-menu" onClick={() => toggle()} />
             </div>
 
         </div>
